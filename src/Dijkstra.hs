8d1aff42a04bed1d3eac4a1244e852860b44391d
James Bowen <james@mondaymorninghaskell.me>
2022-07-23 15:41:00 -0700

    Dijkstra basics

---
+++ src/Dijkstra.hs
@@ -0,0 +1,121 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Dijkstra where
+
+import Data.Array (Array)
+import qualified Data.Array as A
+import Data.Foldable (foldl')
+import Data.Hashable (Hashable)
+import Data.HashMap.Lazy (HashMap)
+import qualified Data.HashMap.Lazy as HM
+import Data.Heap (MaxHeap, MinHeap, MaxPrioHeap, MinPrioHeap)
+import qualified Data.Heap as H
+import Data.Maybe (catMaybes)
+import Data.HashSet (HashSet)
+import qualified Data.HashSet as HS
+
+-- 4. Add tests for that.
+-- 5. Remove necessity to enumerate state space.
+-- 6. Ord instead of Hashable? Benchmark?
+-- 7. Allow both Ord or Hashable?
+
+data GraphDist a = Dist a | Infinity
+  deriving (Eq, Show)
+
+instance (Ord a) => Ord (GraphDist a) where
+  Infinity <= Infinity = True
+  Infinity <= _ = False
+  (Dist _) <= Infinity = True
+  (Dist x) <= (Dist y) = x <= y
+
+addDist :: (Num a) => GraphDist a -> GraphDist a -> GraphDist a
+addDist (Dist x) (Dist y) = Dist (x + y)
+addDist _ _ = Infinity
+
+rawDist :: (Bounded a) => GraphDist a -> a
+rawDist (Dist x) = x
+rawDist Infinity = maxBound
+
+class DijkstraGraph g where
+  type DijkstraNode g :: *
+  type DijkstraDistance g :: *
+  dijkstraNodes :: g -> [DijkstraNode g]
+  dijkstraEdges :: g -> DijkstraNode g -> [(DijkstraNode g, (DijkstraDistance g))]
+
+type DijkstraState n d = (HashSet n, MinPrioHeap (GraphDist d) n, HashMap n (GraphDist d))
+
+findShortestDistance ::
+  forall g. (DijkstraGraph g, Eq (DijkstraNode g), Hashable (DijkstraNode g), Num (DijkstraDistance g), Ord (DijkstraDistance g)) =>
+  g -> DijkstraNode g -> DijkstraNode g -> GraphDist (DijkstraDistance g)
+findShortestDistance graph src dest = processQueue (HS.empty, initialQueue, dist) HM.! dest
+  where
+    initialDistances = (src, Dist 0) : [(n, Infinity) | n <- dijkstraNodes graph, n /= src]
+    dist = HM.fromList initialDistances
+
+    initialQueue :: MinPrioHeap (GraphDist (DijkstraDistance g)) (DijkstraNode g)
+    initialQueue = H.fromList [(d, node) | (node, d) <- initialDistances]
+
+    processQueue :: DijkstraState (DijkstraNode g) (DijkstraDistance g) -> HM.HashMap (DijkstraNode g) (GraphDist (DijkstraDistance g))
+    processQueue (v0, q0, d0) = case H.view q0 of
+      Nothing -> d0
+      -- While heap is not empty
+      -- Withdraw minimum distance/index (d, i@(r, c)) from heap
+      Just ((minDist, coord), q1) -> if HS.member coord v0
+        then processQueue (v0, q1, d0)
+        -- If we have not seen this i already.
+        else
+          let v1 = HS.insert coord v0
+              -- Get all unvisited neighbors of i (j)
+              allNeighbors = dijkstraEdges graph coord
+              unvisitedNeighbors = filter (\(c, _) -> not (HS.member c v1)) allNeighbors
+              -- Update their distances to be the minimum of src->i + i->j or existing dist[j]
+              -- Place that back in the heap
+          in  processQueue $ foldl (foldNeighbor coord) (v1, q1, d0) unvisitedNeighbors
+
+    foldNeighbor ::
+         DijkstraNode g
+      -> DijkstraState (DijkstraNode g) (DijkstraDistance g)
+      -> (DijkstraNode g, (DijkstraDistance g))
+      -> DijkstraState (DijkstraNode g) (DijkstraDistance g)
+    foldNeighbor c1 ds@(v, q, d) (c2, distC1ToC2) =
+      let altDistance = addDist (d HM.! c1) (Dist distC1ToC2)
+      in  if altDistance < d HM.! c2
+            then (v, (H.insert (altDistance, c2) q), (HM.insert c2 altDistance d))
+            else ds
+
+-- Examples
+newtype Graph = Graph
+  { adjacencyList :: HashMap String [(String, Int)]
+  }
+
+instance DijkstraGraph Graph where
+  type DijkstraNode Graph = String
+  type DijkstraDistance Graph = Int
+  dijkstraNodes (Graph adjList) = HM.keys adjList
+  dijkstraEdges (Graph adjList) nodeName = case HM.lookup nodeName adjList of
+    Nothing -> []
+    Just x -> x
+
+newtype Graph2D = Graph2D (A.Array (Int, Int) Int)
+
+instance DijkstraGraph Graph2D where
+  type DijkstraNode Graph2D = (Int, Int)
+  type DijkstraDistance Graph2D = Int
+  dijkstraNodes (Graph2D arr) = A.indices arr
+  dijkstraEdges (Graph2D arr) cell = [(n, arr A.! n) | n <- neighbors]
+    where
+      neighbors = getNeighbors arr cell
+
+getNeighbors :: Array (Int, Int) Int -> (Int, Int) -> [(Int, Int)]
+getNeighbors input (row, col) = catMaybes [maybeUp, maybeDown, maybeLeft, maybeRight]
+  where
+    (maxRow, maxCol) = snd . A.bounds $ input
+    maybeUp = if row > 0 then Just (row - 1, col) else Nothing
+    maybeDown = if row < maxRow then Just (row + 1, col) else Nothing
+    maybeLeft = if col > 0 then Just (row, col - 1) else Nothing
+    maybeRight = if col < maxCol then Just (row, col + 1) else Nothing
---
+++ test/DijkstraTest.hs
@@ -0,0 +1,58 @@
+module Main where
+
+import Data.Array (Array, listArray)
+import qualified Data.Array as A
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HM
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Dijkstra
+import Lib (read2DDigits)
+import Second (augmentRisks)
+
+main :: IO ()
+main = do
+  aocCase1 <- read2DDigits "inputs/day_15_basic.txt"
+  aocCase2 <- read2DDigits "inputs/day_15_final.txt"
+  let aocCase1' = augmentRisks aocCase1
+  let aocCase2' = augmentRisks aocCase2
+  defaultMain $ testGroup "Dijkstra Tests"
+    (dijkstraTests (Graph2D aocCase1, Graph2D aocCase2, Graph2D aocCase1', Graph2D aocCase2'))
+
+dijkstraTests :: (Graph2D, Graph2D, Graph2D, Graph2D) -> [TestTree]
+dijkstraTests (case1, case2, case3, case4) =
+  [ testCase "Dijkstra 2D 1" $ findShortestDistance dijkstraInput2D (0, 0) (4, 4) @?= Dist 14
+  , testCase "Dijkstra 1" $ findShortestDistance dijkstraInputGraph "A" "D" @?= Dist 40
+  , testCase "Dijkstra 2" $ findShortestDistance dijkstraInputGraph2 "A" "D" @?= Dist 32
+  , testCase "AOC Day 15 - 1" $ findShortestDistance case1 (0,0) (9,9) @?= Dist 40
+  , testCase "AOC Day 15 - 2" $ findShortestDistance case2 (0,0) (99,99) @?= Dist 447
+  , testCase "AOC Day 15 - 3" $ findShortestDistance case3 (0,0) (49,49) @?= Dist 315
+  , testCase "AOC Day 15 - 4" $ findShortestDistance case4 (0,0) (499,499) @?= Dist 2825
+  ]
+
+dijkstraInput2D :: Graph2D
+dijkstraInput2D = Graph2D $ listArray ((0, 0), (4, 4))
+  [ 0, 2, 1, 3, 1
+  , 1, 1, 8, 1, 4
+  , 1, 8, 8, 8, 1
+  , 1, 9, 9, 9, 1
+  , 1, 4, 1, 9, 1
+  ]
+
+dijkstraInputGraph :: Graph
+dijkstraInputGraph = Graph $ HM.fromList
+  [ ("A", [("D", 100), ("B", 1), ("C", 20)])
+  , ("B", [("D", 50)])
+  , ("C", [("D", 20)])
+  , ("D", [])
+  ]
+
+dijkstraInputGraph2 :: Graph
+dijkstraInputGraph2 = Graph $ HM.fromList
+  [ ("A", [("D", 100), ("B", 1), ("C", 20)])
+  , ("B", [("D", 50), ("E", 30)])
+  , ("C", [("D", 20)])
+  , ("D", [])
+  , ("E", [("D", 1)])
+  ]
