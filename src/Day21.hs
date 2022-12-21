6261054b45cecdc6adfb864a85f246b125dcf9ea
James <james@mondaymorninghaskell.me>
2022-12-21 14:58:44 -0800

    Day 21 - 1

--- src/Day21.hs
+++ src/Day21.hs
@@ -2,64 +2,181 @@
 
 module Day21 where
 
-import Control.Monad.Logger (MonadLogger, runStdoutLoggingT)
-import Text.Megaparsec (ParsecT, sepEndBy1)
-import Text.Megaparsec.Char (eol)
+import Control.Monad.Logger (MonadLogger, runStdoutLoggingT, logErrorN, logError)
+import Text.Megaparsec (ParsecT, sepEndBy1, some, (<|>))
+import Text.Megaparsec.Char (eol, letterChar, string, char)
 import Data.Void (Void)
-import Data.Text (Text)
+import Data.Text (Text, pack)
 
-import Utils (parseFile)
+import Utils (parseFile, parsePositiveNumber)
+import Data.Int (Int64)
+import qualified Data.HashMap.Strict as HM
 
 dayNum :: Int
 dayNum = 21
 
 -------------------- PUTTING IT TOGETHER --------------------
-solveEasy :: FilePath -> IO (Maybe Int)
+solveEasy :: FilePath -> IO (Maybe Int64)
 solveEasy fp = runStdoutLoggingT $ do
   input <- parseFile parseInput fp
-  result <- processInputEasy input
-  findEasySolution result
+  Just <$> processInputEasy input
 
-solveHard :: FilePath -> IO (Maybe Int)
+solveHard :: FilePath -> IO (Maybe Int64)
 solveHard fp = runStdoutLoggingT $ do
   input <- parseFile parseInput fp
-  result <- processInputHard input
-  findHardSolution result
+  Just <$> processInputHard input
 
 -------------------- PARSING --------------------
-type InputType = ()
+type DependencyMap = HM.HashMap String [String]
+type CalculationMap = HM.HashMap String Calculation
+type InputType = CalculationMap
+
+data Op =
+  Plus |
+  Minus |
+  Times |
+  Divided |
+  Equals
+  deriving (Show, Eq)
+
+data Calculation =
+  FinalValue Int64 |
+  Operation Op String String |
+  HumanVal
+  deriving (Show, Eq)
+
+data Value =
+  SolvedValue Int64 | UnsolvedValue String
+  deriving (Show, Eq)
 
 parseInput :: (MonadLogger m) => ParsecT Void Text m InputType
-parseInput =
-  return ()
+parseInput = HM.fromList <$> sepEndBy1 parseLine eol
 
--- parseInput :: (MonadLogger m) => ParsecT Void Text m InputType
--- parseInput =
---   sepEndyBy1 parseLine eol
+type LineType = (String, Calculation)
 
--- type InputType = [LineType]
--- type LineType = ()
-
--- parseLine :: (MonadLogger m) => ParsecT Void Text m LineType
--- parseLine = return ()
+parseLine :: (MonadLogger m) => ParsecT Void Text m LineType
+parseLine = do
+  name <- some letterChar
+  string ": "
+  calc <- parseFinalValue <|> parseOpNode
+  return (name, calc)
+  where
+    parseFinalValue = FinalValue . fromIntegral <$> parsePositiveNumber
+    parseOp =
+      (char '+' >> return Plus) <|>
+      (char '-' >> return Minus) <|>
+      (char '*' >> return Times) <|>
+      (char '/' >> return Divided)
+    parseOpNode = do
+      s1 <- some letterChar
+      char ' '
+      op <- parseOp
+      char ' '
+      s2 <- some letterChar
+      return $ Operation op s1 s2
 
 -------------------- SOLVING EASY --------------------
-type EasySolutionType = ()
+type EasySolutionType = Int64
 
 processInputEasy :: (MonadLogger m) => InputType -> m EasySolutionType
-processInputEasy _ = undefined
+processInputEasy calculationMap = solveValue calculationMap "root"
+
+  -- Filter Calculation Map By FinalValues
+  -- Plug These into Calculation Map by using dependencyMap
+  -- Filter Calculation Map by Ops with Solved Values
+  -- Replace with Final Values
+  -- Recurse
 
 findEasySolution :: (MonadLogger m) => EasySolutionType -> m (Maybe Int)
 findEasySolution _ = return Nothing
 
+solveValue :: (MonadLogger m) => CalculationMap -> String -> m Int64
+solveValue calculationMap name = case HM.lookup name calculationMap of
+  Nothing -> logErrorN ("Invalid Name: " <> pack name <> "!") >> return minBound
+  Just HumanVal -> logErrorN "Can't solve human value! Check with hasHumanVal first." >> return minBound
+  Just (FinalValue x) -> return x
+  Just (Operation op s1 s2) -> do
+    x1 <- solveValue calculationMap s1
+    x2 <- solveValue calculationMap s2
+    case op of
+      Plus -> return $ x1 + x2
+      Minus -> return $ x1 - x2
+      Times -> return $ x1 * x2
+      Divided -> return $ x1 `quot` x2
+      Equals -> logErrorN "Invalid use of equals...can only apply to 'root'" >> return minBound
+
 -------------------- SOLVING HARD --------------------
 type HardSolutionType = EasySolutionType
 
-processInputHard :: (MonadLogger m) => InputType -> m HardSolutionType
-processInputHard _ = undefined
+updateCalculationsHard :: (MonadLogger m) => CalculationMap -> m CalculationMap
+updateCalculationsHard calculationMap = do
+  let map' = HM.insert "humn" HumanVal calculationMap
+  case HM.lookup "root" calculationMap of
+    Nothing -> logErrorN "Error! Must have root!" >> return calculationMap
+    Just (FinalValue x) -> logErrorN "Error! Root cannot be final!" >> return calculationMap
+    Just HumanVal -> logErrorN "Error! Root cannot be human!" >> return calculationMap
+    Just (Operation _ s1 s2) -> return $ HM.insert "root" (Operation Equals s1 s2) map'
 
-findHardSolution :: (MonadLogger m) => HardSolutionType -> m (Maybe Int)
-findHardSolution _ = return Nothing
+processInputHard :: (MonadLogger m) => InputType -> m HardSolutionType
+processInputHard input = do
+  input' <- updateCalculationsHard input
+  case HM.lookup "root" input' of
+    Nothing -> return minBound
+    Just (FinalValue x) -> return (-4)
+    Just HumanVal -> return (-5)
+    Just (Operation _ s1 s2) -> do
+      human1 <- hasHumanDep input' s1
+      human2 <- hasHumanDep input' s2
+      case (human1, human2) of
+        (True, True) -> logErrorN "Both sides have human deps...I need a new approach." >> return (-1)
+        (False, False) -> logErrorN "Neither side has human dep...probably didn't substitute right." >> return (-2)
+        (True, False) -> do
+          v2 <- solveValue input' s2
+          getHumanValForExpectedOutcome input' v2 s1
+        (False, True) -> do
+          v1 <- solveValue input' s1
+          getHumanValForExpectedOutcome input' v1 s2
+
+getHumanValForExpectedOutcome :: (MonadLogger m) => CalculationMap -> Int64 -> String -> m Int64
+getHumanValForExpectedOutcome calculationMap expected nodeName = do
+  -- logErrorN (pack nodeName)
+  case calculationMap HM.! nodeName of
+    HumanVal -> return expected
+    (FinalValue _) -> logErrorN "This node doesn't actually depend on human value!" >> return (-3)
+    (Operation op s1 s2) -> do
+      human1 <- hasHumanDep calculationMap s1
+      human2 <- hasHumanDep calculationMap s2
+      case (human1, human2) of
+        (True, True) -> logErrorN "Both sides have human deps...I need a new approach." >> return (-1)
+        (False, False) -> logErrorN "Neither side has human dep...probably didn't substitute right." >> return (-2)
+        (True, False) -> do
+          v2 <- solveValue calculationMap s2
+          case op of
+            Plus -> getHumanValForExpectedOutcome calculationMap (expected - v2) s1
+            Minus -> getHumanValForExpectedOutcome calculationMap (expected + v2) s1
+            Times -> getHumanValForExpectedOutcome calculationMap (expected `quot` v2) s1
+            Divided -> getHumanValForExpectedOutcome calculationMap (expected * v2) s1
+            Equals -> logErrorN "Equals is reserved for root." >> return (-6)
+        (False, True) -> do
+          v1 <- solveValue calculationMap s1
+          case op of
+            Plus -> getHumanValForExpectedOutcome calculationMap (expected - v1) s2
+            Minus -> getHumanValForExpectedOutcome calculationMap (v1 - expected) s2
+            Times -> getHumanValForExpectedOutcome calculationMap (expected `quot` v1) s2
+            Divided -> getHumanValForExpectedOutcome calculationMap (expected * v1) s2
+            Equals -> logErrorN "Equals is reserved for root." >> return (-7)
+
+
+hasHumanDep :: (MonadLogger m) => CalculationMap -> String -> m Bool
+hasHumanDep calculationMap nodeName = do
+  let node = calculationMap HM.! nodeName
+  case node of
+    HumanVal -> return True
+    (FinalValue _) -> return False
+    (Operation _ s1 s2) -> do
+      human1 <- hasHumanDep calculationMap s1
+      human2 <- hasHumanDep calculationMap s2
+      return $ human1 || human2
 
 -------------------- SOLUTION PATTERNS --------------------
 
@@ -95,14 +212,14 @@ smallFile = "inputs_2022/day_" <> show dayNum <> "_small.txt"
 largeFile :: FilePath
 largeFile = "inputs_2022/day_" <> show dayNum <> "_large.txt"
 
-easySmall :: IO (Maybe Int)
+easySmall :: IO (Maybe Int64)
 easySmall = solveEasy smallFile
 
-easyLarge :: IO (Maybe Int)
+easyLarge :: IO (Maybe Int64)
 easyLarge = solveEasy largeFile
 
-hardSmall :: IO (Maybe Int)
+hardSmall :: IO (Maybe Int64)
 hardSmall = solveHard smallFile
 
-hardLarge :: IO (Maybe Int)
+hardLarge :: IO (Maybe Int64)
 hardLarge = solveHard largeFile
--- test/TestAoC2022.hs
+++ test/TestAoC2022.hs
@@ -23,7 +23,7 @@ import qualified Day17 as D17
 import qualified Day18 as D18
 import qualified Day19 as D19
 import qualified Day20 as D20
--- import qualified Day21 as D21
+import qualified Day21 as D21
 -- import qualified Day22 as D22
 -- import qualified Day23 as D23
 -- import qualified Day24 as D24
@@ -111,10 +111,10 @@ main = do
   -- day202 <- D20.easyLarge
   -- day203 <- D20.hardSmall
   -- day204 <- D20.hardLarge
---   day211 <- D21.easySmall
---   day212 <- D21.easyLarge
---   day213 <- D21.hardSmall
---   day214 <- D21.hardLarge
+  day211 <- D21.easySmall
+  day212 <- D21.easyLarge
+  day213 <- D21.hardSmall
+  day214 <- D21.hardLarge
 --   day221 <- D22.easySmall
 --   day222 <- D22.easyLarge
 --   day223 <- D22.hardSmall
@@ -212,10 +212,10 @@ main = do
     -- , testCase "Day 20-2" $ day202 @?= Just 11123
     -- , testCase "Day 20-3" $ day203 @?= 1623178306
     -- , testCase "Day 20-4" $ day204 @?= 4248669215955
-    -- , testCase "Day 21-1" $ day211 @?= Just 0
-    -- , testCase "Day 21-2" $ day212 @?= Just 0
-    -- , testCase "Day 21-3" $ day213 @?= Just 0
-    -- , testCase "Day 21-4" $ day214 @?= Just 0
+    , testCase "Day 21-1" $ day211 @?= Just 152
+    , testCase "Day 21-2" $ day212 @?= Just 169525884255464
+    , testCase "Day 21-3" $ day213 @?= Just 301
+    , testCase "Day 21-4" $ day214 @?= Just 3247317268284
     -- , testCase "Day 22-1" $ day221 @?= Just 0
     -- , testCase "Day 22-2" $ day222 @?= Just 0
     -- , testCase "Day 22-3" $ day223 @?= Just 0
