dc392df38b611eee083650331695fe9ed7f82e98
James <james@mondaymorninghaskell.me>
2022-12-22 12:57:12 -0800

    Extensive comments

--- src/Day22.hs
+++ src/Day22.hs
@@ -25,11 +25,12 @@ solveEasy fp = runStdoutLoggingT $ do
   findEasySolution result
 
 solveHard :: String -> FilePath -> IO (Maybe Int)
-solveHard level fp = runStdoutLoggingT $ do
+solveHard size fp = runStdoutLoggingT $ do
   input@((grid, _, _), turns) <- parseFile parseInput fp
-  let (wrapFunc, faceFunc) = if level == "easy" then (wrapEasy, getFaceEasy) else (wrapHard, getFaceHard)
+  -- This problem requires hardcoding between small and large solutions.
+  let (wrapFunc, faceFunc) = if size == "small" then (wrapEasy, getFaceEasy) else (wrapHard, getFaceHard)
   result <- processInputHard ((grid, faceFunc), turns) wrapFunc
-  findEasySolution result
+  findEasySolution result -- < Evaluation solution is same as in the "Easy" part.
 
 -------------------- PARSING --------------------
 data Direction =
@@ -51,41 +52,64 @@ data Cell =
 type MazeInfo = (Grid2 Cell, A.Array Int (Int, Int), A.Array Int (Int, Int))
 type InputType = (MazeInfo, [(Turn, Int)])
 
+-- 1. Parse the cells lines (which adds padding to the front).
+-- 2. Get the maximum column and add padding to the back.
+--      This includes one Blank beyond the final column for every row.
+-- 3. Add an extra line of padding of 'Blank' to the top and bottom.
+-- 4. Construct a 2D Array with the cells.
+--      First element that can be in the maze is (1,1), but Array's index starts at (0,0) for padding.
+-- 5. Make an array out of "rowInfos", which are included from parsing the rows.
+--      These tell us the first and last non-Blank index in each row.
+-- 6. Calculate "columnInfos" based on the maze grid.
+--      These tell us the first and last non-Blank index in each column.
+-- 7. Parse the path.
 parseInput :: (MonadLogger m, MonadFail m) => ParsecT Void Text m InputType
 parseInput = do
-  -- Parse all the lines (pad front of every line with a single blank), track the row info.
+  {- 1 -}
   cellLines <- sepEndBy1 parseLine eol
   let maxColumn = maximum (snd . snd <$> cellLines)
-  let paddedCellLines = map (\(cells, (_, lastNonBlankIndex)) -> cells ++ replicate (maxColumn - lastNonBlankIndex + 1) Blank) cellLines
-  let topBottom = replicate (maxColumn + 2) Blank
-  let finalCells = concat (topBottom : paddedCellLines) ++ topBottom
-  let maze = A.listArray ((0, 0), (length paddedCellLines + 1, maxColumn + 1)) finalCells
-  let rowInfos = A.listArray (1, length cellLines) (snd <$> cellLines)
-  let columns = map (calculateColInfo maze) [1..maxColumn]
-  let columnInfos = A.listArray (1, maxColumn) columns
+{-2-} paddedCellLines = map (\(cells, (_, lastNonBlankIndex)) -> cells ++ replicate (maxColumn - lastNonBlankIndex + 1) Blank) cellLines
+{-3-} topBottom = replicate (maxColumn + 2) Blank
+      finalCells = concat (topBottom : paddedCellLines) ++ topBottom
+{-4-} maze = A.listArray ((0, 0), (length paddedCellLines + 1, maxColumn + 1)) finalCells
+{-5-} rowInfos = A.listArray (1, length cellLines) (snd <$> cellLines)
+{-6-} columns = map (calculateColInfo maze) [1..maxColumn]
+      columnInfos = A.listArray (1, maxColumn) columns
   eol
+  {-7-}
   firstLength <- parsePositiveNumber
   path <- parsePath [(TurnRight, firstLength)]
   return ((maze, rowInfos, columnInfos), path)
   where
+    {- 6 -}
     calculateColInfo :: Grid2 Cell -> Int -> (Int, Int)
     calculateColInfo maze col =
       let nonBlankAssocs = filter (\((r, c), cell) -> c == col && cell /= Blank) (A.assocs maze)
           sorted = sort $ fst . fst <$> nonBlankAssocs
       in  (head sorted, last sorted)
 
+-- Parse the directions.
+-- This function is recursive. It runs until we encounter 'eof'.
 parsePath :: (MonadLogger m, MonadFail m) => [(Turn, Int)] -> ParsecT Void Text m [(Turn, Int)]
 parsePath accum = finished <|> notFinished
   where
-    finished = eof >> return (reverse accum)
+    finished = eof >> return (reverse accum) {- Base Case: End-of--File -}
     notFinished = do
       t <- (char 'R' >> return TurnRight) <|> (char 'L' >> return TurnLeft)
       i <- parsePositiveNumber
-      parsePath ((t, i) : accum)
+      parsePath ((t, i) : accum) {- Recursive Case: Add the new turn and distance. -}
 
--- type InputType = [LineType]
+-- Parse a single line of maze input.
+-- '.' = Empty space in the maze
+-- '#' = A wall in the maze
+-- ' ' = A 'Blank' space that is not part of the maze.
+--
+-- In addition to the list of cells, this also returns the start and end
+-- column of the non-blank spaces.
+--
+-- NOTE: This function adds an extra 'Blank' to the front of the
+--       row because we want to pad all 4 directions.
 type LineType = ([Cell], (Int, Int))
-
 parseLine :: (MonadLogger m, MonadFail m) => ParsecT Void Text m LineType
 parseLine = do
   cells <- some parseCell
@@ -103,21 +127,31 @@ type EasySolutionType = (Coord2, Direction)
 processInputEasy :: (MonadLogger m) => (MazeInfo, [(Turn, Int)]) -> m EasySolutionType
 processInputEasy (info@(maze, _, _), directions) = runMoves info (start, FaceUp) directions
   where
-    start = fst $ head $ sortOn (snd . fst) $ filter (\((row, _), cell) -> row == 1 && cell == Empty) (A.assocs maze)
+    -- The initial position in the maze
+    start :: Coord2
+    -- 1. Get all maze indices that are empty in Row 1
+    -- 2. Sort by the column (snd)
+    -- 3. Pick the first
+    start = head $ {-3-}
+{-2-} sortOn snd $
+{-1-} filter (\c@(row, _) -> row == 1 && maze A.! c == Empty) (A.indices maze)
 
+-- Recursively run all the moves.
+-- With each call, process one element of 'directions' - turn once and move the set number of times.
 runMoves :: (MonadLogger m) => MazeInfo -> (Coord2, Direction) -> [(Turn, Int)] -> m (Coord2, Direction)
-runMoves _ final [] = return final
+runMoves _ final [] = return final {- Base Case - No more turns moves. -}
 runMoves info (currentLoc, currentDir) ((nextTurn, distance) : rest) = do
   finalCoord <- runMovesTail distance currentLoc
-  runMoves info (finalCoord, newDir) rest
+  runMoves info (finalCoord, newDir) rest {- Recursive -}
   where
     newDir = turn nextTurn currentDir
 
+    -- Recursively move the given direction a set number of times.
     runMovesTail :: (MonadLogger m) => Int -> Coord2 -> m Coord2
-    runMovesTail 0 c = return c
+    runMovesTail 0 c = return c {- Base Case - n=0 -}
     runMovesTail n c = do
-      c' <- move info (c, newDir)
-      runMovesTail (n - 1) c'
+      result <- move info (c, newDir)
+      runMovesTail (n - 1) result {- Recursive Case (n - 1) -}
 
 turn :: Turn -> Direction -> Direction
 turn TurnLeft d = case d of
@@ -131,21 +165,27 @@ turn TurnRight d = case d of
   FaceDown -> FaceLeft
   FaceLeft -> FaceUp
 
+-- 1. Get the next coordinate based on our direction
+-- 2. If the next coordinate is empty, move there. If it's a wall, return the old location.
+-- 3. If it's blank, wrap around to the next cell.
+--      This requires checking the 'rowInfo' for horizontal wrapping, and the 'columnInfo' for vertical wrapping.
 move :: (MonadLogger m) => MazeInfo -> (Coord2, Direction) -> m Coord2
 move (maze, rowInfo, columnInfo) (loc@(row, column), direction) = return nextCell
   where
+    {- 1 -}
     nextCoords = case direction of
       FaceUp -> (row - 1, column)
       FaceRight -> (row, column + 1)
       FaceDown -> (row + 1, column)
       FaceLeft -> (row, column - 1)
     nextCell = case maze A.! nextCoords of
-      Wall -> loc
-      Empty -> nextCoords
+      Wall -> loc {- 2 -}
+      Empty -> nextCoords {- 2 -}
       Blank -> if maze A.! nextCellWrapped == Empty
         then nextCellWrapped
         else loc
 
+    {- 3 -}
     nextCellWrapped = case direction of
       FaceUp -> (snd $ columnInfo A.! column, column)
       FaceRight -> (row, fst $ rowInfo A.! row)
@@ -162,6 +202,10 @@ findEasySolution ((row, column), direction) = return $ Just (1000 * row + 4 * co
       FaceUp -> 3
 
 -------------------- SOLVING HARD --------------------
+-- What changes in Part 2 is the presence of the "Face Function" mapping each coordinate
+-- to a numbered face, and the "Wrap Function" telling us where to go when we wrap around.
+-- See the "Face and Wrapping Functions" section below for the hardcoded implementations of these.
+
 type HardSolutionType = Int
 type Face = Int
 type MazeInfoHard = (Grid2 Cell, Coord2 -> Face)
@@ -180,6 +224,8 @@ runMovesHard info wrap (currentLoc, currentDir) ((nextTurn, distance) : rest) = 
   where
     newDir = turn nextTurn currentDir
 
+    -- Unlike part 1, our direction can change without us "turning", so this function
+    -- needs to return a new coordinate and a new direction.
     runMovesTail :: (MonadLogger m) => Int -> (Coord2, Direction) -> m (Coord2, Direction)
     runMovesTail 0 c = return c
     runMovesTail n (c, d) = do
@@ -201,10 +247,13 @@ moveHard (maze, getFace) wrap (loc@(row, column), direction) = return result
         then (nextCellWrapped, nextDirWrapped)
         else (loc, direction)
 
+    {- Primary difference comes with this logic, see functions below. -}
     (nextCellWrapped, nextDirWrapped) = wrap loc (getFace loc) direction
 
 --------------- Face and Wrapping Functions ---------
 
+-- getFace functions map
+-- These are specific to the shape of the inputs (day_22_small.txt and day_22_large.txt)
 getFaceEasy :: Coord2 -> Face
 getFaceEasy (row, col)
   | row <= 4 = 1
@@ -223,6 +272,30 @@ getFaceHard (row, col)
   | row <= 150 = 6
   | otherwise = 4
 
+-- NOTE: I use a different numbering for the faces than the example given in the problem description online (https://www.adventofcode.com/2022/day/22)
+-- My numbering reflects how the faces connect on a standard six-sided die (observe that 1 is opposite 6, 2 to 5, and 3 to 4).
+-- So for the smaller example, my numbering looks like this:
+--         1111
+--         1111
+--         1111
+--         1111
+-- 555533332222
+-- 555533332222
+-- 555533332222
+-- 555533332222
+--         66664444
+--         66664444
+--         66664444
+--         66664444
+
+-- type WrapFunction = Coord2 -> Face -> Direction -> (Coord2, Direction)
+-- Given:
+--   1.) Starting Coordinate
+--   2.) The face of the square we're on (1-6)
+--   3.) The Direction we're facing
+--
+-- Find the new coordinate and direction, presuming we go off the edge.
+-- These are hard-coded based on how I observed to fold the inputs (day_22_small.txt and day_22_large.txt)
 wrapEasy :: WrapFunction
 wrapEasy (row, col) face dir = case (face, dir) of
   (1, FaceLeft) -> ((5, row + 4), FaceDown) 
@@ -273,7 +346,7 @@ easyLarge :: IO (Maybe Int)
 easyLarge = solveEasy largeFile
 
 hardSmall :: IO (Maybe Int)
-hardSmall = solveHard "easy" smallFile
+hardSmall = solveHard "small" smallFile
 
 hardLarge :: IO (Maybe Int)
-hardLarge = solveHard "hard" largeFile
+hardLarge = solveHard "large" largeFile
